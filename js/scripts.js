cc7ca8060533cbac72f078fab378ec2e4484ef35
AudreyCherrie <mitchelaudrey@gmail.com>
2019-10-11 12:52:04 +0300

    update the js

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+
+var modal = document.getElementById('id0');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  }
+  function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  }
